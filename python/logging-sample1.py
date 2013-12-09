f7e8f54ce2bcdd6a252fedf16bc1474b3890acc3
= <=>
2013-12-09 08:25:29 -0500

    new logging and watchdog library examples

---
+++ python/logging-sample1.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+#!-*- coding=UTF-8 -*-
+import logging
+
+logging.basicConfig(format='%(asctime)s\t%(message)s')
+logging.debug('i am a debug message')
+logging.info('i am a info message')
+logging.warn('i am a warn message')
+logging.error('i am a error message')
