e46451ac30f398e40378a8b9a349b25b883b9d88
Liuchang0812 <liuchang0812@gmail.com>
2013-12-17 17:32:35 +0800

    uintxx_t

---
+++ cpp/type.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+
+int main(){
+    uint64_t totalSize = 0;
+    int32_t i = 1212;
+    std::cout << totalSize<< ' ' << sizeof(totalSize) << std::endl;
+    std::cout << i << ' ' << sizeof(i) << std::endl;
+    return 0;
+}
