27487cfab0f4b28079e5a4d4e24f50330114cffb
Liuchang0812 <liuchang0812@gmail.com>
2013-12-25 19:23:53 +0800

    Add curses-sample1.py

---
+++ python/curses-sample1.py
@@ -0,0 +1,16 @@
+#! /usr/bin/env python
+#-*- coding=UTF-8 -*-
+
+import curses
+curses.initscr();
+
+pad = curses.newpad(100,100)
+for y in range(0, 100):
+    for x in range(0, 100):
+        try:
+            pad.addch(y,x, ord('a') + (x*x + y*y) % 26)
+        except curses.error:
+            pass
+pad.refresh(0,0,5,5,20,75)
+
+
