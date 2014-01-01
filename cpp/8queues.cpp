f1b5d28d8e692c648963987b909e4c5aa7557922
Liuchang0812 <liuchang0812@gmail.com>
2014-01-01 22:42:17 +0800

    add 8queues.cpp

---
+++ cpp/8queues.cpp
@@ -0,0 +1,47 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+
+typedef long long ll;
+
+ll sum = 0;
+int map[9];
+
+#define QUEUE_NUM  8
+
+bool check(int x,int y)
+{
+  int ok = true;
+  for(int i = 0;i < x;i ++)
+  {
+     if( map[i] == y ) return false;
+     if( x -i == abs( y - map[i] ) ) return false;
+  }
+  return true;
+}
+   
+
+void try_nth(int lineno)
+{
+    if ( lineno == QUEUE_NUM )
+    {
+        sum ++;
+        return;
+    }
+    for(int i = 0;i < QUEUE_NUM;i ++)
+    {
+        if( check(lineno, i) )
+        {
+            map[lineno] = i;
+            try_nth(lineno+1);
+            map[lineno] = -1;
+        }
+    }
+}
+
+int main()
+{
+    try_nth(0);
+    printf("%lld\n", sum);
+    return 0;
+}
