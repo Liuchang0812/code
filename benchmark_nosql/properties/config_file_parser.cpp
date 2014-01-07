cbecb75b74e99abd5a079bc312d5ba07f290ebcc
Liuchang0812 <liuchang0812@gmail.com>
2014-01-07 17:41:22 +0800

    Corect config_file_parser.cpp

--- benchmark_nosql/properties/config_file_parser.cpp
+++ benchmark_nosql/properties/config_file_parser.cpp
@@ -1,34 +1,37 @@
+#include <iostream>
 #include <fstream>
 #include <string>
+#include <map>
+
+using namespace std;
 
 class ConfigFileParser{
     public:
         ConfigFileParser(string filename);
         ~ConfigFileParser();
-        int parserFile(const map<string, string> &result);
+        int parserFile(map<string, string> &result);
     private:
         string filename_;
 };
 
-ConfigFileParser::ConfigFileParser(string filename)
+ConfigFileParser::ConfigFileParser(std::string filename):filename_(filename)
 {
-    filename_ = filename;
 }
 ConfigFileParser::~ConfigFileParser()
 {
 }
-ConfigFileParser::parserFile(const map<string, string> &result)
+int ConfigFileParser::parserFile(map<string, string> &result)
 {
-    if (filename_ == NULL || filename_ == "")
+    if (filename_ == "")
     {
         throw "filename is empty!";
-        return;
+        return -1;
     }
-    ifstream cin(filename_);
+    ifstream cin(filename_.c_str());
     string buffer;
-    while(getline(cin, buffer) != EOF)
+    while( getline(cin, buffer) )
     {
-        std::cerr << "processing: "<< buffer << endl;
+        cerr << "processing: "<< buffer << endl;
         int position = buffer.find("=");
         if( position == -1)
         {
@@ -36,5 +39,6 @@ ConfigFileParser::parserFile(const map<string, string> &result)
         }
         result[buffer.substr(0,position)] = buffer.substr(position+1);
     }
+    return result.size();
 }
 
