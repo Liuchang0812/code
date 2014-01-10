1949fc46009ab679bfeb8d4ed9794bbee7a89e01
Liuchang0812 <liuchang0812@gmail.com>
2014-01-10 20:42:49 +0800

    Add test-stack.cpp

--- cpp/test-stack.cpp
+++ cpp/test-stack.cpp
@@ -1,5 +1,8 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<vector>
+#include<string>
+using namespace std;
 
 int* f()
 {
@@ -10,11 +13,35 @@ int* f()
     return tmp;
 }
 
+vector<int> testVector()
+{
+    vector<int> tmp;
+    tmp.push_back(1);
+    tmp.push_back(2);
+    tmp.push_back(3);
+    return tmp;
+}
+string testString()
+{
+    string tmp;
+    tmp = "123";
+    return tmp;
+}
+const char* testCharArr()
+{
+    return "123";
+}
+
 int main()
 {
     int* p;
     p = f();
     for (int i = 0;i < 12;i ++)
         printf("%d\n", *p+i);
+    vector<int> t_vec = testVector();
+    for ( int i = 0;i < t_vec.size();i ++)
+        printf("%d\n", t_vec[i]);
+    printf("%s\n",testString().c_str());
+    printf("%s\n",testCharArr());
     return 0;
 }
